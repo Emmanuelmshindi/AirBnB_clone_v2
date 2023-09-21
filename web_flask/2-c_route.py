30b141f42241367462806a30711d226d784ab42a
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-09-21 21:48:53 +0000

    Task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""This script that starts a Flask web application"""
+""" Script with two routes """
+
 from flask import Flask
+from markupsafe import escape
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """ Prints Hello HBNB """
+    return "Hello HBNB!"
 
-@app.route('/')
-def hello_hbnb():
-    """print web"""
-    return 'Hello HBNB!'
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """ Prints HBNB """
+    return "HBNB"
 
+@app.route("/c/<name>", strict_slashes=False)
+def sntence(name):
+    """ Prints c <name> """
+    return "C {}".format(escape(name).replace("_", " "))
 
-@app.route('/hbnb')
-def hbnb():
-    """print web"""
-    return 'HBNB'
-
-
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """print C followed by the value of the text variable"""
-    return 'C {}'.format(text.replace('_', ' '))
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
