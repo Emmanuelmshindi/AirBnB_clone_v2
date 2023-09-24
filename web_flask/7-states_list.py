3b4ce60b058bb6d7b10699cfad4fb8761830aef6
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-09-24 05:50:32 +0000

    States list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,23 +8,27 @@ Routes:
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
+# Create an instance of the Flask class and assign to app variable
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
+# Function to remove sqlalchemy session after each request
 @app.teardown_appcontext
-def close_db(exc):
+def teardown(exception):
     """This close the current session of sqlalchemist"""
     storage.close()
 
-
+# Route to return all state objects
 @app.route('/states_list')
 def states_list():
     """
     This Displays an HTML page with a list of all State objects in DBStorage.
     States are sorted by name.
     """
+    # Fetch all states from storage engine used
     states = storage.all(State).values()
+    # Return html page with states list passed onto it
     return render_template("7-states_list.html", states=states)
 
 
