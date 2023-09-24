d447dcf7084e684504a5c7b199e9675fbfe3576a
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-09-24 19:45:02 +0000

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-""" This Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
-"""
+""" Starts a flask web application """
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# Creates an instance of the flask app and assigns it to the variable app
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-
+# Teardown the sqlalchemy session after each request
 @app.teardown_appcontext
-def closedb(exc):
-    """This is to close a database session"""
+def teardown(exception):
     storage.close()
 
+# Define the route for /cities_by_states
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_state():
+    """ Return state objects with cities related to each state """
+    # Fetch all State objects from DBStorage
+    states = storage.all(State)
 
-@app.route('/cities_by_states')
-def states_list():
-    """ /states_list route """
-    states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
-
+    # Render template and pass list of states to it
+    return render_template("8-cities_by_states.html", states=states)
 
 if __name__ == '__main__':
     storage.reload()
-    app.run("0.0.0.0", 5000)
+    app.run(host="0.0.0.0", port=5000)
